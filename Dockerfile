b97422aec7b2066f52336459dccb4a93a258fa1f
taqwim05 <05taqwim@gmail.com>
2024-12-08 09:55:18 +0700

    hu

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM node:18.17.1
 WORKDIR /app
-ENV PORT 8080
+ENV PORT 9090
 ENV MODEL_URL 'https://storage.googleapis.com/peyimpanan321/model/model.json'
 ENV GOOGLE_PROJECT_ID 'submission-444018'
 COPY . .
 RUN npm install
-EXPOSE 8080
 CMD [ "npm", "run", "start"]
--- src/server/server.js
+++ src/server/server.js
@@ -6,7 +6,7 @@ const InputError = require('../exceptions/InputError');
  
 (async () => {
     const server = Hapi.server({
-        port: process.env.PORT || 8080,
+        port: process.env.PORT || 9000,
         routes: {
             cors: {
               origin: ['*'],
